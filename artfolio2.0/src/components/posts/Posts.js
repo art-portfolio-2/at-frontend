6401da693e4123f5561ce41acdf8b757d022279e
Javontay McElroy <dinomightnetwork@gmail.com>
2019-02-07 16:02:03 -0600

    Styled the CSS

--- artfolio2.0/src/components/posts/Posts.js
+++ artfolio2.0/src/components/posts/Posts.js
@@ -25,7 +25,7 @@ class Posts extends Component {
 
     return (
       <div className="container">
-        <p>user: {this.props.user.username}</p>
+        <p className="card-title">user: {this.props.user.username}</p>
         {
             this.props.posts.map( post => (<Post key={post.id} post={post} deletePost={this.deletePost} updating={this.updating} />))
         }
