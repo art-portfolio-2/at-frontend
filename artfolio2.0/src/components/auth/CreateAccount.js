6e7e54aea826605ab24014ae07bea942121fcd78
Angel Torres <alas4224@gmail.com>
2019-02-07 16:53:14 -0600

    made styling changes to create account form

--- artfolio2.0/src/components/auth/CreateAccount.js
+++ artfolio2.0/src/components/auth/CreateAccount.js
@@ -31,33 +31,33 @@ class CreateAccount extends Component {
             <form onSubmit={this.createAccount} className="col s12">
                 <div className="row">
                     <div className="input-div">
-                        <input onChange={this.handleChanges} value={this.state.username} name="username" type="text" className="validate"/>
                         <label>Username</label>
+                        <input onChange={this.handleChanges} value={this.state.username} name="username" type="text" className="validate"/>
                     </div>
                     <div className="input-div">
-                        <input onChange={this.handleChanges} value={this.state.password} name="password" type="password" className="validate"/>
                         <label>Password</label>
+                        <input onChange={this.handleChanges} value={this.state.password} name="password" type="password" className="validate"/> 
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-div">
-                    <input onChange={this.handleChanges} value={this.state.fullName} name="fullName" type="text" className="validate"/>
                     <label>Full Name</label>
+                    <input onChange={this.handleChanges} value={this.state.fullName} name="fullName" type="text" className="validate"/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-div">
-                        <input onChange={this.handleChanges} value={this.state.email} name="email" type="email" className="validate"/>
                         <label>Email</label>
+                        <input onChange={this.handleChanges} value={this.state.email} name="email" type="email" className="validate"/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="input-div">
-                        <input onChange={this.handleChanges} value={this.state.userImgUrl} name="userImgUrl" type="text" className="validate"/>
                         <label>User Image Url</label>
+                        <input onChange={this.handleChanges} value={this.state.userImgUrl} name="userImgUrl" type="text" className="validate"/>
                     </div>
                 </div>
-                <button className="waves-effect waves-light btn #00695c teal darken-3">Create Account</button>
+                <button className="create-account-btn waves-effect waves-light btn #00695c teal darken-3">Create Account</button>
             </form>
     </div>
         )
